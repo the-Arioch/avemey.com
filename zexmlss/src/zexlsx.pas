c6bf308c2c8f781bd87a644d9312232b782712c2
the-Arioch <the_Arioch@nm.ru>
2013-03-20 19:31:26 +0400

    XLSX: GUI sheet Tabs: move to the right to avoid overlapping with single long-title sheet

--- zexmlss/src/zexlsx.pas
+++ zexmlss/src/zexlsx.pas
@@ -3958,7 +3958,7 @@ begin
     _xml.Attributes.Add('showHorizontalScroll', 'true', false);
     _xml.Attributes.Add('showSheetTabs', 'true', false);
     _xml.Attributes.Add('showVerticalScroll', 'true', false);
-    _xml.Attributes.Add('tabRatio', '200', false);
+    _xml.Attributes.Add('tabRatio', '300', false); // with long titles 200 causes an ugly overlap
     _xml.Attributes.Add('windowHeight', '8192', false);
     _xml.Attributes.Add('windowWidth', '16384', false);
     _xml.Attributes.Add('xWindow', '0', false);
